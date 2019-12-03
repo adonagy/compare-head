be8b97ac2d9a36afc45976dc6e20a5c885f5ed5a
Adrian Nagy <adrian.nagy@simplestaking.com>
2019-12-03 18:49:41 +0000

    rework to integration test

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,38 @@
 #[macro_use]
 extern crate assert_json_diff;
 
-extern crate hyper;
+extern crate reqwest;
 extern crate serde;
-use hyper::{Client};
-use futures::stream::TryStreamExt;
+use std::error::Error;
+use std::io::Read;
 use std::str;
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
-
 #[derive(Debug)]
-enum NodeType {
+pub enum NodeType {
     Tezedge,
     Ocaml,
 }
 
-#[tokio::main]
-async fn main() -> Result<()>{
-    let rust_head = get_head(NodeType::Tezedge).await?;
-    let ocaml_head = get_head(NodeType::Ocaml).await?;
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-    // TODO: make it more obvious in the output
-    //              actual      expected
-    assert_json_eq!(rust_head, ocaml_head);
+fn main() {}
 
-    Ok(())
-}
-
-async fn get_head(node_type: NodeType) -> Result<serde_json::value::Value> {
-    let client = Client::new();
-
+pub fn get_head(node_type: NodeType) -> Result<serde_json::value::Value> {
     let url = match node_type {
-        NodeType::Ocaml => "http://46.101.160.245:3000/chains/main/blocks/head",       // reference Ocaml node
-        NodeType::Tezedge => "http://localhost:18732/chains/main/blocks/head",         // locally built Tezedge node
+        NodeType::Ocaml => "http://46.101.160.245:3000/chains/main/blocks/head", // reference Ocaml node
+        NodeType::Tezedge => "http://localhost:18732/chains/main/blocks/head", // locally built Tezedge node
     };
 
     // let url = format!("http://http://46.101.160.245:{}/chains/main/blocks/head", port);
 
-    let mut res = client.get(url.parse().unwrap()).await?;
-    let body = res.body_mut().try_concat().await?.to_vec();
+    let mut res = reqwest::blocking::get(url)?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
 
-    let json_string = match str::from_utf8(&body) {
-        Ok(v) => v,
-        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-    };
+    // let json_string = match str::from_utf8() {
+    //     Ok(v) => v,
+    //     Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    // };
 
-    Ok(serde_json::from_str(json_string)?)
+    Ok(serde_json::from_str(&body)?)
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,16 +1,20 @@
 #[macro_use]
 extern crate assert_json_diff;
 
-extern crate compare-head;
+extern crate compare_head;
 
 #[test]
-fn test_heads() -> Result<()>{
-    let rust_head = get_head(compare-head::NodeType::Tezedge).await?;
-    let ocaml_head = get_head(compare-head::NodeType::Ocaml).await?;
+fn test_heads() {
+    let rust_head = match compare_head::get_head(compare_head::NodeType::Tezedge) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid json: {}", e),
+    };
+    let ocaml_head = match compare_head::get_head(compare_head::NodeType::Ocaml) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid json: {}", e),
+    };
 
     // TODO: make it more obvious in the output
     //              actual      expected
     assert_json_eq!(rust_head, ocaml_head);
-
-    Ok(())
 }
